03e2606a8f3d6f7ae3c2e4ecc2d156f17432219e
Nicolas Thery <nthery@gmail.com>
2021-01-02 17:06:26 +0100

    cli can now interpret files

--- src/bin/rlox.rs
+++ src/bin/rlox.rs
@@ -1,14 +1,41 @@
-//! Basic read-evaluate-print loop.
+//! Lox interpreter command-line.
+//!
+//! When called without argument it drops into an interactive read-evaluate-print loop.
+//!
+//! When called with arguments, it interprets the corresponding files in a single interpreter
+//! session (so code and data sharing is possible).
 
+use std::env;
+use std::fs::File;
 use std::io;
 use std::io::prelude::*;
+use std::io::BufReader;
+
+use anyhow::{self, Context};
 
 use rlox::interpreter::Interpreter;
 
-fn main() {
-    if let Err(e) = run_prompt() {
-        eprintln!("fatal error: {}", e);
+fn main() -> Result<(), anyhow::Error> {
+    let args = env::args().skip(1).collect::<Vec<_>>();
+    if args.len() > 0 {
+        run_all_files(args)?;
+    } else {
+        run_prompt()?;
     }
+    Ok(())
+}
+
+fn run_all_files(paths: Vec<String>) -> Result<(), anyhow::Error> {
+    let mut interp_stdout = io::stdout();
+    let mut interp = Interpreter::new(&mut interp_stdout);
+
+    for p in &paths {
+        let reader =
+            BufReader::new(File::open(p).with_context(|| format!("failed to open {}", p))?);
+        interp.eval(reader)?;
+    }
+
+    Ok(())
 }
 
 fn run_prompt() -> Result<(), io::Error> {
